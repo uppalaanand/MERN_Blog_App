de9fa5f13e719fda3d834f779b2c64b8bec52a65
Uppala Anand <uppalaanand410@gmail.com>
2026-05-13 11:49:24 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ config();   //process.env
 
 const app = Express();
 
-app.use(cors({ origin : ["http://localhost:5173"], credentials:true}));
+app.use(cors({ origin : ["http://localhost:5173", "mern-blog-app-gamma.vercel.app"], credentials:true}));
 // app.use(cors());
 //add body parser middleware
 app.use(Express.json());
